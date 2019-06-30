e27c92eca0b3c6c33495202783c43b60ecad39e8
Luiz Soares <luizotaviosoares@outlook.com>
2019-06-30 19:01:39 -0300

    finish mobile

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -11,72 +11,75 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 export default class Feed extends Component {
-    state = {
-        feed: []
-    };
+  state = {
+    feed: []
+  };
 
-    async componentDidMount() {
-        this.registerToSocket();
-        const response = await api.get('posts');
-    
-        this.setState({ feed: response.data});
-    }
+  async componentDidMount() {
+    this.registerToSocket();
+    const response = await api.get('posts');
 
-    registerToSocket = () => {
-        const socket = io('http://localhost:3333');
+    this.setState({ feed: response.data });
+  }
 
-        socket.on('post', newPost => {
-            
-            this.setState(prevState => {
-                return{
-                    feed: [newPost]
-                }
-            }, console.log(this.state));
-        });
-        // socket.on('like', newPost => {
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
 
-        // });
-    }
+    socket.on('post', newPost => {
+      this.setState(prevState => {
+        return {
+          feed: [newPost, ...prevState.feed]
+        };
+      });
+    });
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post => (post._id === likedPost._id ? likedPost : post))
+      });
+    });
+  };
 
-    handleLike = id => {
-        api.post(`/posts/${id}/like`);
-    }
-    
-    render() {
-        return(
-            <section id="post-list">
-                { this.state.feed.map(post => (
-                    <article key={post._id}>
-                    <header>
-                        <div className="user-info">
-                            <span>{post.author}</span>
-                            <span className="place">{post.place}</span>
-                        </div>
-                        <img src={more} alt="mais" />
-                    </header>
-                    <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
+  };
 
-                    <footer>
-                        <div className="actions"> 
-                            <button type="button" onClick={() => {
-                                this.handleLike(post._id);
-                            }}>
-                                <img src={like} alt="" />
-                            </button>
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
+  render() {
+    return (
+      <section id="post-list">
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="mais" />
+            </header>
+            <img id="post-image" src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-                        <strong>{post.likes} curtidas</strong>
-                        <p>
-                            {post.description}
-                            <span>{post.hashtags}</span>
-                        </p>
-                    </footer>
-                    </article>
-                ))}
+            <footer>
+              <div className="actions">
+                <button
+                  type="button"
+                  onClick={() => {
+                    this.handleLike(post._id);
+                  }}
+                >
+                  <img src={like} alt="" />
+                </button>
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
 
-            </section>
-        );
-    }
+              <strong>{post.likes} curtidas</strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
+      </section>
+    );
+  }
 }
